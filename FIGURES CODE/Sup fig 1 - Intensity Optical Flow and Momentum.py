8b1f37c4cede5ea1ddea2e9aed11986c9992baf7
Ariel Rom <166027157+AriRom123@users.noreply.github.com>
2026-09-03 15:25:29 -0400

    Update Sup fig 1 - Intensity Optical Flow and Momentum.py

--- FIGURES CODE/Sup fig 1 - Intensity Optical Flow and Momentum.py
+++ FIGURES CODE/Sup fig 1 - Intensity Optical Flow and Momentum.py
@@ -158,6 +158,16 @@ class FlowAnalyze:
             plt.tight_layout()
             plt.show()
 
+        #means = np.mean(np.array(all_convergence), axis=0)
+        #plt.figure(figsize=(8, 5))
+        #plt.plot(means, linewidth=2)
+        #plt.xlabel("Iteration")
+        #plt.ylabel("Mean flow update (L2 norm)")
+        #plt.title("Horn–Schunck Convergence")
+        #plt.grid(True, alpha=0.3)
+        #plt.xlim([0,150])
+        #plt.tight_layout()
+        #plt.show()
 
     def calculate_waveness(self, type):
 
@@ -796,7 +806,7 @@ class Display:
 
         if data_type == 'cortex':
             brain_mask = np.load('brain_mask.npy')
-            outer_line_rgb = np.load(outer_line_rgb.npy')
+            outer_line_rgb = np.load('outer_line_rgb.npy')
 
             ax.imshow(outer_line_rgb)
             ax3.imshow(outer_line_rgb)
